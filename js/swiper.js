cab69ad3bdd9ebfa2f099147bea09a4b21937950
ys <busmaida@gmail.com>
2026-09-04 18:08:30 +0900

    메인페이지

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,27 @@
-const swiper = new Swiper('.swiper', {
+const heroBanner = new Swiper('.hero-banner', {
   loop: true,
   autoplay: {
     delay: 6000,
   },
+});
+
+const bestSellerProducts = new Swiper('.best-seller-products',{
+  breakpoints: {
+    0: {
+      slidesPerView: 1.5,
+      spaceBetween: 12
+    },
+    340: {
+      slidesPerView: 2.5,
+      spaceBetween: 12
+    },
+    768: {
+      slidesPerView: 3.5,
+      spaceBetween: 12
+    },
+    1024: {
+      slidesPerView: 4.5,
+      spaceBetween: 14
+    }
+  }
 });
