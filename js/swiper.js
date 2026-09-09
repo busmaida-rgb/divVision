fedf505e373a3294179e3c34c4b31f3baa9f9c2a
ys <busmaida@gmail.com>
2026-09-09 12:29:14 +0900

    자잘수정

--- js/swiper.js
+++ js/swiper.js
@@ -5,24 +5,28 @@ const heroBanner = new Swiper('.hero-banner', {
   autoplay: {
     delay: 6000,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
 const bestSellerProducts = new Swiper('.best-seller-products',{
   breakpoints: {
     0: {
-      slidesPerView: 1.5,
+      slidesPerView: 1.7,
       spaceBetween: 12
     },
     340: {
-      slidesPerView: 2.5,
+      slidesPerView: 2.7,
       spaceBetween: 12
     },
     768: {
-      slidesPerView: 3.5,
+      slidesPerView: 3.7,
       spaceBetween: 12
     },
     1024: {
-      slidesPerView: 4.5,
+      slidesPerView: 4.7,
       spaceBetween: 14
     }
   }
